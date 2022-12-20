217e39eaa0ffefe1a6e52aa61322de57a7e1b986
maphy9 <112250835+maphy9@users.noreply.github.com>
2022-12-20 11:10:21 +0200

    123

--- script.js
+++ script.js
@@ -18,17 +18,3 @@ function showPop() {
         }
     }
 }
-
-var string = "Happy 2023 year!";
-
-function parseInteger() {
-    var num = "";
-
-    for (const i in string) {
-        if (string[i]%2 != Nan) {
-            num += string[i];
-        } else {
-            continue;
-        }
-    }
-}
